167ef93886185004c870f9675199686ad3081974
hukutu4 <dxza@ya.ru>
2024-04-07 13:59:59 +0300

    fix

--- env_loader.sh
+++ env_loader.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 #set -x  # Uncomment for debug mode
-source ~/.profile
 
 # Function for loading .env file
-load_env() {
-  while read -r line; do
-    # Skip empty lines and lines starting with #
-    if [[ "$line" = \#* ]] || [ -z "$line" ]; then
-  	continue
-    fi
-    export "$line"
-  done < .env
-}
+while read -r line; do
+  # Skip empty lines and lines starting with #
+  if [[ "$line" = \#* ]] || [ -z "$line" ]; then
+    continue
+  fi
+  export "$line"
+done < .env
